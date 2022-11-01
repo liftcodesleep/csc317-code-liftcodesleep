e071adbbe2d36bafac87909f1d5bf51ba7aaa1c1
slippy <you@example.com>
2022-10-31 22:52:19 -0700

    too much

--- application/public/js/registration.js
+++ application/public/js/registration.js
@@ -14,7 +14,6 @@ function pwValidation() {
   ) {
     error += "Username must begin with a letter.<br>";
   }
-
   else if (username.length < 3 || !aplhanum.test(username)) {
     error +=
       "Username must include 3 or more alphanumeric characters.<br>";
@@ -24,36 +23,28 @@ function pwValidation() {
   var num = /[0-9]/;
   var uppercase = /[A-Z]/;
   var specialchar = /[/*-+!@#$^&*]/;
-
   if (password.length < 8) {
     console.log("I am in length test");
     error += "Password must be 8 or more characters.<br>";
   }
-
   else if (!uppercase.test(password)) {
     error += "Password must contain at least one uppercase Letter.";
   }
-
   else if (!num.test(password)) {
     console.log("I am in at least one number test");
     error += "Password must contain at least one number.<br>";
   }
-
-
   else if (!specialchar.test(password)) {
     error +=
       "Password must contain one of the following characters: /*-+!@#$^&*.<br>";
   }
-
   else if (password !== passValidation) {
     error += "Both passwords must be identical!";
   } else {
     passwordCheck = true;
   } if (passwordCheck && usernameCheck) {
-
     document.getElementById("myForm").submit();
   } else {
-
     response.innerHTML = error;
     response.style.color = "red";
   }
