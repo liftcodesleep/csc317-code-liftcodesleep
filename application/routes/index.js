0a8bd5b6f85b5eb7be3e046d42a94b609b04cd93
slippy <you@example.com>
2022-11-04 15:44:28 -0700

    milestone one push

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'CSC 317 App', name: "Jacob Lawrence" });
+  res.sendFile(index.html);
 });
 
 module.exports = router;
