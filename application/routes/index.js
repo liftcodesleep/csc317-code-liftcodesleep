e0d55f33d9994d67e05966d1df3bde742027b530
slippy <you@example.com>
2022-11-05 22:10:56 -0700

    hotspot keeps dying smh

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,12 +3,19 @@ const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function (req, res, next) {
-//   res.render('index', { title: 'CSC 317 App', name: "Jacob Lawrence" });
-// });
-router.get('/index', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'CSC 317 App', name: "Jacob Lawrence" });
+});
+router.get('/registration', function (req, res) {
+  res.sendFile(path.join(__dirname + '/registration.html'));
   //__dirname : It will resolve to your project folder.
 });
+router.get("/postimage", function (req, res) {
+  res.render('postimage');
+});
+router.get("posts/:id(//d+)", function (req, res) {
+  res.render('viewpost');
+});
+
 
 module.exports = router;
