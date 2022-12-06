53b88f50e65b87e886aff12c31e34ae883bafff7
slippy <you@example.com>
2022-12-05 21:09:51 -0800

    can make posts

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -1,11 +1,46 @@
 const express = (require('express'));
 const router = express.Router();
+const multer = require('multer');
+const sharp = require('sharp');
 
 const db = require('../conf/database');
-
-router.post("/create", function (req, res, next) {
-  console.log(req);
-  res.send();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    let fileExt = file.mimetype.split("/")[1];
+    cb(null, `${file.fieldname} - ${Date.now()}-${Math.round(Math.random() * 1E9)}.${fileExt}`)
+  }
 })
+
+const upload = multer({ storage: storage })
+router.post("/create", upload.single("uploadImage"), function (req, res, next) {
+  let uploadedFile = req.file.path;
+  let thumbnailName = `thumbnail-${req.file.filename}`;
+  let destinationOfThumbnail = `${req.file.destination}/${thumbnailName}`;
+  const { title, description } = req.body;
+  const userId = req.session.userId;
+
+  sharp(uploadedFile)
+    .resize(200)
+    .toFile(destinationOfThumbnail)
+    .then(function () {
+      let baseSQL = `
+    INSERT INTO posts (title, description, image, thumbnail, fk_authorId) VALUE (?,?,?,?,?)`
+      return db.query(baseSQL, [title, description, uploadedFile, destinationOfThumbnail, userId])
+    })
+    .then(function ([results, fields]) {
+      if (results && results.affectedRows) {
+        req.flash("success", "Your post has been published!");
+        req.session.save(function (saveErr) {
+          res.redirect('/');
+        })
+      }
+    })
+    .catch(err => next(err));
+})
+
 
 module.exports = router;
